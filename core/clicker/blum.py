586136421fe0d28d01d02cc9444bbcfa097a0915
Makhlenko Sergey <mahlenko-weblive@yandex.ru>
2024-11-13 01:31:02 +0300

    Test sleep

--- core/clicker/blum.py
+++ core/clicker/blum.py
@@ -185,5 +185,7 @@ class BlumClicker:
                 self.detect_replay(screenshot, rect)
                 self.detect_reload_screen(screenshot)
 
+                await asyncio.sleep(0.1)
+
         except (Exception, ExceptionGroup) as error:
             logger.error(get_language("WINDOW_CLOSED").format(error=error))
