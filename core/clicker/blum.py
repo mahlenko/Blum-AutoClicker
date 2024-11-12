2b9238bf0659f9e87af5e80b8e0ef13182d82106
Makhlenko Sergey <mahlenko-weblive@yandex.ru>
2024-11-13 00:06:56 +0300

    Update range

--- core/clicker/blum.py
+++ core/clicker/blum.py
@@ -60,7 +60,7 @@ class BlumClicker:
 
         use_freezing = get_config_value("USE_FREEZING")
 
-        for x, y in product(range(0, width, 10), range(120, height, 10)):
+        for x, y in product(range(0, width, 5), range(140, height, 5)):
             r, g, b = screen.getpixel((x, y))
 
             has_flower = self.detect_color_range((r, g, b), (208, 216, 0))
